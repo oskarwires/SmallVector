8c1f115466f5669f40c14c980491990512b3cb3f
oskarwires <oskar>
2025-04-19 17:49:45 +0100

    Working

--- test.cpp
+++ test.cpp
@@ -14,23 +14,43 @@ int main() {
     std::cout << std::endl;
   }
 
+  my_vec.shrink_to_fit();
+  std::cout << "new capacity after shrink: " << my_vec.capacity() << "\n";
+  std::cout << "\n";
+
   size_t vec_size = my_vec.size();
 
+  // Read with operator[]
   for (size_t i = 0; i < vec_size; i++) {
     std::cout << my_vec[i] << " ";
   }
-
   std::cout << "\n";
 
+  // Read with .at()
   for (size_t i = 0; i < vec_size; i++) {
     std::cout << my_vec.at(i) << " ";
   }
+  std::cout << "\n";
 
+  // Read with forward iterator
+  auto end = my_vec.end();
+  for (auto it = my_vec.begin(); it != end; ++it) {
+    std::cout << *it << " ";
+  }
+  std::cout << "\n";
+
+  // Read with reverse iterator
+  auto rend = my_vec.rend();
+  for (auto it = my_vec.rbegin(); it != rend; ++it) {
+    std::cout << *it << " ";
+  }
   std::cout << "\n";
 
   assert(my_vec.front() == 0);
   assert(my_vec.back() == 9);
   assert(my_vec.data()[2] == 2);
+  assert(*my_vec.begin() == 0);
+  assert(*(my_vec.end() - 1) == 9);
 
   my_vec.clear();
   assert(my_vec.size() == 0);
--- vec.hpp
+++ vec.hpp
@@ -1,18 +1,18 @@
-#include <array>
+#pragma once
+
 #include <cassert>
+#include <iterator>
 #include <limits>
 #include <optional>
 #include <vector>
 
-#include <iostream>
-
-template <class T, size_t STATIC_SIZE = 8> class SmallVector {
+template <class T, size_t STATIC_AMOUNT = 8> class SmallVector {
 private:
   using VecT = std::vector<T>;
 
   // When our arr_ overflows, we move into the internal vec_
   // TODO: Decide, do we move back into vector if unspilled?
-  std::array<T, STATIC_SIZE> arr_;
+  alignas(alignof(T)) std::byte arr_[STATIC_AMOUNT * sizeof(T)];
   std::optional<VecT> vec_;
   size_t size_;
 
@@ -21,17 +21,26 @@ private:
   void spillover(size_t capacity) {
     assert(!vec_); // Should be uninitialised
     assert(capacity >=
-           STATIC_SIZE); // Should be more than we are moving or same
+           STATIC_AMOUNT); // Should be more than we are moving or same
     vec_ = std::make_optional<VecT>();
     vec_->reserve(capacity);
     for (size_t i = 0; i < size_; i++) {
-      vec_->push_back(std::move(arr_[i]));
+      vec_->push_back(std::move(get_element(i)));
+      get_element_ptr(i)->~T();
     }
   }
+
+  // Just two nice helper functions
+  T *get_element_ptr(size_t i) noexcept {
+    return reinterpret_cast<T *>(arr_) + i;
+  }
+  T &get_element(size_t i) noexcept { return *get_element_ptr(i); }
 
 public:
   using iterator = T *;
   using const_iterator = const T *;
+  using reverse_iterator = std::reverse_iterator<iterator>;
+  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
   SmallVector() : size_(0) {};
 
@@ -53,8 +62,10 @@ public:
     }
   }
 
-  T &operator[](size_t i) { return vec_ ? (*vec_)[i] : arr_[i]; }
-  const T &operator[](size_t i) const { return vec_ ? (*vec_)[i] : arr_[i]; }
+  T &operator[](size_t i) { return vec_ ? (*vec_)[i] : get_element(i); }
+  const T &operator[](size_t i) const {
+    return vec_ ? (*vec_)[i] : get_element(i);
+  }
 
   T &front() { return (*this)[0]; }
   const T &front() const { return (*this)[0]; }
@@ -62,43 +73,45 @@ public:
   T &back() { return (*this)[size_ - 1]; }
   const T &back() const { return (*this)[size_ - 1]; }
 
-  T *data() noexcept { return vec_ ? &(*vec_)[0] : &arr_[0]; }
-  const T *data() const noexcept { return vec_ ? &(*vec_)[0] : &arr_[0]; }
+  T *data() noexcept { return vec_ ? &(*vec_)[0] : get_element_ptr(0); }
+  const T *data() const noexcept {
+    return vec_ ? &(*vec_)[0] : get_element_ptr(0);
+  }
 
   // ----- ITERATORS -----
 
-  iterator begin() noexcept { return vec_ ? &(*vec_)[0] : &arr_[0]; }
+  iterator begin() noexcept { return vec_ ? &(*vec_)[0] : get_element_ptr(0); }
   const_iterator begin() const noexcept {
-    return vec_ ? &(*vec_)[0] : &arr_[0];
+    return vec_ ? &(*vec_)[0] : get_element_ptr(0);
   }
   const_iterator cbegin() const noexcept {
-    return vec_ ? &(*vec_)[0] : &arr_[0];
+    return vec_ ? &(*vec_)[0] : get_element_ptr(0);
   }
 
-  iterator end() noexcept { return vec_ ? &(*vec_)[size_] : &arr_[size_]; }
+  iterator end() noexcept {
+    return vec_ ? &(*vec_)[size_] : get_element_ptr(size_);
+  }
   const_iterator end() const noexcept {
-    return vec_ ? &(*vec_)[size_] : &arr_[size_];
+    return vec_ ? &(*vec_)[size_] : get_element_ptr(size_);
   }
   const_iterator cend() const noexcept {
-    return vec_ ? &(*vec_)[size_] : &arr_[size_];
+    return vec_ ? &(*vec_)[size_] : get_element_ptr(size_);
   }
 
-  iterator rbegin() noexcept {
-    return vec_ ? &(*vec_)[size_ - 1] : &arr_[size_ - 1];
+  reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
+  const_reverse_iterator rbegin() const noexcept {
+    return const_reverse_iterator(end());
   }
-  const_iterator rbegin() const noexcept {
-    return vec_ ? &(*vec_)[size_ - 1] : &arr_[size_ - 1];
-  }
-  const_iterator crbegin() const noexcept {
-    return vec_ ? &(*vec_)[size_ - 1] : &arr_[size_ - 1];
+  const_reverse_iterator crbegin() const noexcept {
+    return const_reverse_iterator(cend());
   }
 
-  iterator rend() noexcept { return vec_ ? &(*vec_)[-1] : &arr_[-1]; }
-  const_iterator rend() const noexcept {
-    return vec_ ? &(*vec_)[-1] : &arr_[-1];
+  reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
+  const_reverse_iterator rend() const noexcept {
+    return const_reverse_iterator(begin());
   }
-  const_iterator crend() const noexcept {
-    return vec_ ? &(*vec_)[-1] : &arr_[-1];
+  const_reverse_iterator crend() const noexcept {
+    return const_reverse_iterator(cbegin());
   }
 
   // ----- CAPACITY -----
@@ -117,7 +130,7 @@ public:
   // internal vector and reserving size if size > STATIC_SIZE
   void reserve(size_t size) {
     // If we want the amount statically stored, then, do nothing!
-    if (size <= STATIC_SIZE) {
+    if (size <= STATIC_AMOUNT) {
       return;
     } else {
       if (!vec_) {
@@ -134,7 +147,7 @@ public:
   // capacity
   size_t capacity() const noexcept {
     if (!vec_) {
-      return STATIC_SIZE;
+      return STATIC_AMOUNT;
     } else {
       return vec_->capacity();
     }
@@ -150,31 +163,44 @@ public:
 
   // ----- MODIFIERS -----
 
+  void clear() {
+    if (vec_) {
+      // All the static elements have been moved to the vector, and already
+      // destructed by `spillover()`, so all we need to do is clear the vec
+      vec_->clear();
+    } else {
+      for (size_t i = size_; i > 0; --i) {
+        get_element_ptr(i - 1)->~T();
+      }
+    }
+    size_ = 0;
+  }
+
   // Pushes data to the back of our SmallVector
   void push_back(T &&val) {
     if (!vec_) {
-      if (size_ >= STATIC_SIZE) {
+      if (size_ >= STATIC_AMOUNT) {
         spillover(size_ + 1);
         vec_->push_back(std::forward<T>(val));
       } else {
-        arr_[size_] = std::forward<T>(val);
+        new (get_element_ptr(size_)) T(std::forward<T>(val));
       }
     } else {
       vec_->push_back(std::forward<T>(val));
     }
     size_++;
   }
-
-  void clear() {
-    if (vec_) {
-      // All the static elements have been moved to the vector, so they don't
-      // need to be destructed, only the vector needs to be cleared
-      vec_->clear();
-    } else {
-      for (auto it = vec_->rbegin(); it != vec_->rend(); ++it) {
-        std::destroy_at(&it);
+  void push_back(const T &val) {
+    if (!vec_) {
+      if (size_ >= STATIC_AMOUNT) {
+        spillover(size_ + 1);
+        vec_->push_back(val);
+      } else {
+        new (get_element_ptr(size_)) T(val);
       }
+    } else {
+      vec_->push_back(val);
     }
-    size_ = 0;
+    size_++;
   }
 };
